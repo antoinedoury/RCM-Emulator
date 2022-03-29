05aabade8594e4633e2124b00699bd1c1774a4d1
antoinedoury <73891940+antoinedoury@users.noreply.github.com>
2022-03-29 10:09:36 +0200

    Update make_unet.py

--- make_unet.py
+++ make_unet.py
@@ -1,3 +1,10 @@
+#########################################
+####  EMUL-UNET ARCHITECTURE DESIGNER ### 
+#########################################
+
+# This file propose a main function to create the UNET architecture used for the Emulator introduced in Doury et al. (2022). 
+# We work here with Keras and Tensorflow
+
 import keras
 import tensorflow as tf 
 from keras import backend as K
@@ -11,11 +18,13 @@ from keras.optimizers import Adam
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 from keras.models import Sequential
 
+### We first define some function which are useful for the rest 
 
+# The RMSE loss
 def rmse_k(y_true, y_pred):
     return keras.backend.sqrt(keras.backend.mean(keras.backend.square(y_pred - y_true), axis=-1))
 
-#Let's define a basic CNN with few convolutions and MaxPooling
+#A basic CNN with few convolutions and MaxPooling : 
 def block_conv(conv, filters):
     conv = Conv2D(filters, 3, padding='same')(conv)
     conv = BatchNormalization()(conv)
@@ -25,40 +34,46 @@ def block_conv(conv, filters):
     conv = Activation('relu')(conv)
     return conv
 
+# A UP-scaling block used in the decoding part. This block also concatenate with the output of the decoding part.  
 def block_up_conc(conv, filters,conv_conc):
     conv = Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding='same', activation='relu')(conv)
     conv = concatenate([conv,conv_conc])
     conv = block_conv(conv, filters)
     return conv
 
+# An other UP-scaling block with no concatenation as our UNET expand the decoding part. 
 def block_up(conv, filters):
     conv = Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding='same', activation='relu')(conv)
     conv = block_conv(conv, filters)
     return conv
 
-
+# A quick function to get the highest power of two close to n.
 def highestPowerof2(n):
     res = 0;
     for i in range(n, 0, -1):
-         
         # If i is a power of 2
         if ((i & (i - 1)) == 0):
-         
             res = i;
             break;
-         
     return res;
 
+### This is the function drawing the UNET. It is designed to adapt to any size of inputs and outputs maps. 
+### To recall : the emulator proposed in Doury et al (2022) takes two sources of inputs : a set of 2D variables and a 1D vector. 
+### This function also build the Emul-UNET with only the 2D variables as input. 
+### This is set with the variable "nb_inputs" : 1 or 2 sources of inputs.
+### 
+### The function needs the size of the output map ( "size_target_domain" ). 
+### And the shape of the inputs as a list of lists: must be under the form [[width of 2D var,height of 2D var,nb of 2D var],[1,1, nb_of_1D_var]] if nb_inputs=2 and
+### [[width of 2D var,height of 2D var,nb of 2D var]] if nb_inputs=1.
+### The function returns a Keras model. 
 
-def unet_maker( nb_inputs,size_target_domain,shape_inputs, filters = 64, OROG=False,seed=123):
+def unet_maker( nb_inputs,size_target_domain,shape_inputs, filters = 64,seed=123):
     from math import log2,pow
     import os
     import numpy as np
-    import random as rn
     inputs_list=[]
     size=np.min([highestPowerof2(shape_inputs[0][0]),highestPowerof2(shape_inputs[0][1])])
-   
-    
+
     if nb_inputs==1:
         inputs = keras.Input(shape = shape_inputs[0])
         conv_down=[]
@@ -104,7 +119,6 @@ def unet_maker( nb_inputs,size_target_domain,shape_inputs, filters = 64, OROG=Fa
             model2 = Dense(filters*int(pow(2,i)))(model2)
     
         merged = concatenate([last_conv,model2])
-        #up=concatenate([pool_last,conv_down[int(log2(size_input_domain)-1)]])
         up=merged
         k=log2(size)
         for i in range(1,int(log2(size_target_domain)+1)):
@@ -115,14 +129,7 @@ def unet_maker( nb_inputs,size_target_domain,shape_inputs, filters = 64, OROG=Fa
                 up=conv
         inputs_list.append(inputs)
         inputs_list.append(inputs2)
-    if OROG:
-        inputs3 = keras.Input(shape= [64,64,1])
-        conv_orog = block_conv(inputs3, filters)
-        concate=concatenate([up,conv_orog])
-        last=block_conv(concate, filters)
-        inputs_list.append(inputs3)
-    else:
-        last=up
+    last=up
         
     lastconv=Conv2D(1, 1, padding='same')(last)
     return (keras.models.Model(inputs=inputs_list, outputs=lastconv))
